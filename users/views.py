cfd510a11fc4468d646ed663c1dbec89d2cb6183
Evgeny <monevalex@mail.ru>
2021-10-14 17:01:29 +0400

    lesson4

--- users/views.py
+++ users/views.py
@@ -1,19 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegisterForm
 
 
 def login(request):
-    form = UserLoginForm
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+        else:
+            print(form.errors)
+    else:
+        form = UserLoginForm()
     content = {
         'title': 'Geekshop - Авторизация',
         'form': form
     }
     return render(request, 'users/login.html', content)
 
+
 def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:login'))
+        else:
+            print(form.errors)
+    else:
+        form = UserRegisterForm()
     content = {
-        'title': 'Geekshop - Регистрация'
+        'title': 'Geekshop - Регистрация',
+        'form': form
     }
     return render(request, 'users/register.html', content)
 
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
