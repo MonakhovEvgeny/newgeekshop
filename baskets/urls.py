20c01a9f3b8c9c97de857ad698ec8690fa8e6348
Evgeny <monevalex@mail.ru>
2021-10-17 20:44:01 +0400

    divine recovery from full ass

--- baskets/urls.py
+++ baskets/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-app_name = 'baskets'
+# from baskets.views import basket_add,basket_remove,basket_edit
+from baskets.views import basket_add
 
-# urlpatterns = [
-#     path('login/', login, name='login'),
-#     path('register/', register, name='register'),
-#     path('logout/', logout, name='logout'),
-#     path('profile/', profile, name='profile')
-# ]
+app_name = 'baskets'
+urlpatterns = [
+    path('add/<int:product_id>/', basket_add, name='basket'),
+    # path('remove/<int:product_id>/',basket_remove,name='basket_remove'),
+    # path('edit/<int:id>/<int:quantity>/',basket_edit,name='basket_edit')
+    ]
